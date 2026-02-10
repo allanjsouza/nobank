e150ccf238c23629da0b041698bd7667f5b5cc02
Allan J. Souza <allanjsouza@gmail.com>
2026-02-10 17:55:22 -0300

    chore(migration): add users table

---
+++ priv/repo/migrations/20260210195622_add_users_table.exs
@@ -0,0 +1,14 @@
+defmodule Nobank.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table("users") do
+      add :name, :string, null: false
+      add :password_hash, :string, null: false
+      add :email, :string, null: false
+      add :postal_code, :string, null: false
+
+      timestamps()
+    end
+  end
+end
