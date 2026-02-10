f227fb368f65672f38570dec50ce36b34e866bae
Allan J. Souza <allanjsouza@gmail.com>
2026-02-10 16:54:54 -0300

    chore: add Credo dep and config

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Nobank.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
