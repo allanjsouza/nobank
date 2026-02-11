5d8216778ede463f31cac3e8e7f022c9dd5b9642
Allan J. Souza <allanjsouza@gmail.com>
2026-02-11 18:53:39 -0300

    chore: add User module and schema

---
+++ lib/nobank/users/user.ex
@@ -0,0 +1,23 @@
+defmodule Nobank.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @required_params [:name, :password, :email, :postal_code]
+
+  schema "users" do
+    field :name, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    field :email, :string
+    field :postal_code, :string
+
+    timestamps()
+  end
+
+  def changeset(user \\ %__MODULE__{}, params) do
+    user
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:name, min: 3)
+  end
+end
