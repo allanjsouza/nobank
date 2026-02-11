b93888df76ddf472eede4745e5ad78e3e66ad1fd
Allan J. Souza <allanjsouza@gmail.com>
2026-02-11 18:54:57 -0300

    chore: ensure users have password hash with argon2

--- lib/nobank/users/user.ex
+++ lib/nobank/users/user.ex
@@ -2,6 +2,8 @@ defmodule Nobank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
   @required_params [:name, :password, :email, :postal_code]
 
   schema "users" do
@@ -19,5 +21,12 @@ defmodule Nobank.Users.User do
     |> cast(params, @required_params)
     |> validate_required(@required_params)
     |> validate_length(:name, min: 3)
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: pwd}} = changeset) do
+    put_change(changeset, :password_hash, Argon2.hash_pwd_salt(pwd))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
